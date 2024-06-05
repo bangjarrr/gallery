0170c52f9a99d0a90e999231217d201f3ae21f37
bangjarrr <alfajjar16@gmail.com>
2024-06-05 08:40:47 +0700

    rawr

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import NavbarComp from './navbar';
 import App from './App';
 import FaqComp from './footer';
 import './main.css';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="w-body">
+      <SpeedInsights />
       <NavbarComp />
       <App />
       <FaqComp />
